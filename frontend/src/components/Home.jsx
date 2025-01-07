d06f79cbf5cd665783c72260e0126fe3500b5935
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-01-07 11:08:11 +0200

    basic navigation

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,20 +5,41 @@ const Home = () => {
     const { jwtData } = useAuth();
 
     useEffect(() => {
-      if (jwtData) {
-        console.log('JWT Data:', jwtData);
-      }
+        if (jwtData) {
+            console.log('JWT Data:', jwtData);
+        }
     }, [jwtData]);
 
     return (
         <section>
             <nav>
                 <img src="../../public/logo.png" alt="logo" />
+                <ul>
+                    {jwtData ? (
+                        <>
+                            <li>
+                                <a href="/contact">Contact</a>
+                            </li>
+                            <li>
+                                <a href="/aboutus">About Us</a>
+                            </li>
+                            <li>
+                                <a href="/logout">Logout</a>
+                            </li>
+                        </>
+                    ) : (
+                        <li>
+                            <a href="/">Login</a>
+                        </li>
+                    )}
+
+
+                </ul>
             </nav>
             {jwtData ? (
                 <h2>Welcome, {jwtData.user.first_name}!</h2>
             ) : (
-                <p>No JWT data found</p>
+                <p>You are not logged in</p>
             )}
         </section>
     );
--- frontend/src/loader.js
+++ frontend/src/loader.js
@@ -1,38 +1,37 @@
 import './index.css';
-import { redirect } from 'react-router-dom';
 
 
 export const handleGoogleCallback = async (context) => {
-  // Exchange callback's code for JWT tokens
+    // Exchange callback's code for JWT tokens
 
-  const { request } = context;
-  const url = new URL(request.url);
-  const code = url.searchParams.get('code');
+    const { request } = context;
+    const url = new URL(request.url);
+    const code = url.searchParams.get('code');
 
-  if (code) {
-    try {
-      const response = await fetch('http://localhost:8000/api/v1/auth/google/', {
-        method: 'POST',
-        body: JSON.stringify({ code }),
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
+    if (code) {
+        try {
+            const response = await fetch('http://localhost:8000/api/v1/auth/google/', {
+                method: 'POST',
+                body: JSON.stringify({ code }),
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
 
-      const jwtData = await response.json();
-      console.log(jwtData);
+            const jwtData = await response.json();
+            // Debug
+            // console.log(jwtData);
 
-      if (!response.ok) {
-        throw new Error(jwtData.error || 'Failed to fetch JWT data');
-      }
+            if (!response.ok) {
+                throw new Error(jwtData.error || 'Failed to fetch JWT data');
+            }
+            return jwtData;
 
-      return jwtData;
-      
-    } catch (err) {
-      console.error(err);
-      throw new Response('Bad request', { status: 400 });
+        } catch (err) {
+            console.error(err);
+            throw new Response('Bad request', { status: 400 });
+        }
     }
-  }
 
-  throw new Response('Not Found', { status: 404 });
+    throw new Response('Not Found', { status: 404 });
 };
