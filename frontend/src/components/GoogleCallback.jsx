f64e4fb5487d2a1d103057b6c4932f55b82683f3
xaoccc <114498517+xaoccc@users.noreply.github.com>
2025-01-11 10:47:30 +0200

    login

--- frontend/src/components/GoogleCallback.jsx
+++ frontend/src/components/GoogleCallback.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import React, { useEffect } from 'react';
+import appRoutes from '../core/routes/routes.js';
 
 const GoogleCallback = () => {
   const jwtData = useLoaderData(); // Get the jwtData returned from the loader
@@ -11,7 +12,7 @@ const GoogleCallback = () => {
   React.useEffect(() => {
     if (jwtData) {
       setJwtData(jwtData); // Update the AuthContext with jwtData
-      navigate('/home'); // Redirect to the Home route
+      navigate(appRoutes.HOME); // Redirect to the Home route
     }
   }, [jwtData, setJwtData, navigate]);
 
